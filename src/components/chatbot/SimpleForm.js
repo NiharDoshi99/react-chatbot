b3ecc80e5bae14fcf478aaaa46960abb9b3237c1
Nihar Doshi <nihardoshi16@gmail.com>
2022-03-02 15:20:09 +0530

    intial commit

---
+++ src/components/chatbot/SimpleForm.js
@@ -0,0 +1,44 @@
+import React, {Component} from "react";
+import Chatbot from "react-simple-chatbot";
+
+class SimpleForm extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            inputUser : [],
+        }
+    }
+
+    render() {
+        return (
+            
+                <Chatbot 
+                userDelay='600'
+                botDelay='500'
+                cache
+                enableMobileAutoFocus
+                steps={[
+                    {
+                        id:'intro', 
+                        message:'Hi I am Chatty bot . How can I help you ? ', 
+                        trigger:'intro-user1',
+                       },
+                       {
+                        id: 'intro-user1',
+                        user: true,
+                        trigger: 'loop',
+                       },
+                       {
+                           id: 'loop',
+                           message: '{previousValue}',
+                           trigger: 'intro-user1'
+                       },
+                ]}
+                />
+            
+        );
+    }
+}
+
+export default SimpleForm;
